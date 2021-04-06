2b716c79499bd7592ff553a875df92e828b3664d
Arup Kar <57253029+Arupkarbabu@users.noreply.github.com>
2021-04-06 22:00:50 +0530

    Add files via upload

---
+++ os_module.js
@@ -0,0 +1,7 @@
+const os = require("os");
+var totalmemory = os.totalmem();
+var freememory = os.freemem();
+//console.log("total memory" + totalmemory);
+
+console.log(`total memory ${totalmemory}`);
+console.log(`total memory ${freememory}`);
